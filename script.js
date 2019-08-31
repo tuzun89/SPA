a3ba3a87ac16ffc855fb870c4258f587989fcd88
Tugay <to.uzunoglu@gmail.com>
2019-09-01 00:30:24 +0100

    fixed js bug

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var slides = document.querySelectorAll('#images .image');
+var slides = document.querySelectorAll('#images .img');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,2000);
 
 function nextSlide() {
-    slides[currentSlide].className = 'image';
-    currentSlide = (currentSlide+1)%image.length;
+    slides[currentSlide].className = 'img';
+    currentSlide = (currentSlide+1)%slides.length;
     slides[currentSlide].className = 'current image';
 }
