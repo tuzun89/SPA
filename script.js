e1366a007d0aaf6eec9d5edae6ad311f393e35e7
Tugay <to.uzunoglu@gmail.com>
2019-09-01 01:18:33 +0100

    additional tweaks

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ var slideInterval = setInterval(nextSlide,2000);
 function nextSlide() {
     slides[currentSlide].className = 'slide';
     currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
+    slides[currentSlide].className = 'slide_showing';
 }
